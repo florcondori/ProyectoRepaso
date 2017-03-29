af16215dfe3ca25b425431849243af1af9c13e85
flor condori <condorihuamanif@gmail.com>
2017-03-29 10:35:29 -0500

    page de bienvenida creada

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-app
+window.addEventListener('load',function(){
+	var registrar = document.getElementById('registrar');
+	var login = document.getElementById('login');
+
+	registrar.addEventListener('click',function(e){
+		window.location = "registrarUsuario.html";
+	});
+
+	login.addEventListener('click',function(e){
+
+		window.location = "loginUsuario.html";
+	});
+});
